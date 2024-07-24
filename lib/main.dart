7c19c2341be8f14325020bdc1ab613864de5d411
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2024-07-25 00:15:43 +0200

    add packages and init localization files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale = const Locale('ar');
+
+  Future<void> setLocale(Locale locale) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _locale = locale;
+    });
+    prefs.setString("langCode", locale.languageCode.toString());
+  }
+
+  getLocalLang() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? localCode = prefs.getString('langCode');
+    if (localCode == 'en') {
+      setState(() {
+        _locale = const Locale('en');
+        setLocale(const Locale('en'));
+      });
+    } else {
+      setState(() {
+        _locale = const Locale('ar');
+        setLocale(const Locale('ar'));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale(
+          'en',
+        ),
+        Locale(
+          'ar',
+        ),
+      ],
+      locale: _locale,
+      title: 'Respeto',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -42,7 +93,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-     
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
@@ -64,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
